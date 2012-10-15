7a255f841f8f96d10bbbd038113a5e5764cf9c25
Max Maton <thexa4@gmail.com>
2012-10-15 22:17:14 +0200

    Added camera instance to game

--- pixelplanet.py
+++ pixelplanet.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 import pygame, sys, os
 from pygame.locals import *
-import drawing
+from drawing.camera import Camera
 
 pygame.init()
 
 window = pygame.display.set_mode((1280,720))
 pygame.display.set_caption('Pixel Planet')
+
+camera = Camera(1280,720)
 
 def input(events):
 	for event in events:
