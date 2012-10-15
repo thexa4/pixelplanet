35c191b08b37cdd928c3d5c8a062b732e46f123a
Xenotos <ncherckenrath@gmail.com>
2012-10-16 00:29:19 +0200

    Expanded basic renderer.

--- renderer/renderer.py
+++ renderer/renderer.py
@@ -7,4 +7,13 @@ class Renderer:
 		self.camera = camera
 	
 	def draw(self, world):
-			
+		worldarray = world.2darray()
+		pxarray = pygame.PixelArray(self.camera.window)
+		#Slice worldarray to fit in pxarray using camera
+		for y in range(len(pxarray)):
+			for x in range(len(pxarray[y])):
+				pxarray[y][x] = worldarray[y][x]
+		
+		newwindow = pxarray.make_surface()
+		self.camera.window.blit(newwindow, newwindow.get_rect())
+		pygame.display.flip()
