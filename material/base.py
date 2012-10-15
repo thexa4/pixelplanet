46f889252063cef582a31bea94edd3501f93764b
Max Maton <thexa4@gmail.com>
2012-10-15 23:11:57 +0200

    Fixed syntax error, added color property

--- material/base.py
+++ material/base.py
@@ -1,8 +1,11 @@
-class Base
+import pygame
+
+class Base:
 	"""A base material, this material does nothing"""
 	id = -1
 	mass = 1
 	transparency = 0.0
+	color = pygame.Color("white")
 	
 	def update(world, position):
-		
+		return 0
